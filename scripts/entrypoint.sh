ce49a6adbcf662a2c919f8118d553f661e8a99b8
bio-boris <bio-boris@users.noreply.github.com>
2018-04-05 18:52:30 -0500

    Update entrypoint.sh

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,6 +4,10 @@
 
 python ./scripts/prepare_deploy_cfg.py ./deploy.cfg ./work/config.properties
 
+if [ -f ./work/token ] ; then
+  export KB_AUTH_TOKEN=$(<./work/token)
+fi
+
 if [ $# -eq 0 ] ; then
   sh ./scripts/start_server.sh
 elif [ "${1}" = "test" ] ; then
@@ -12,7 +16,7 @@ elif [ "${1}" = "test" ] ; then
 elif [ "${1}" = "async" ] ; then
   sh ./scripts/run_async.sh
 elif [ "${1}" = "init" ] ; then
-  make ref-data
+  echo "Initialize module"
 elif [ "${1}" = "bash" ] ; then
   bash
 elif [ "${1}" = "report" ] ; then
